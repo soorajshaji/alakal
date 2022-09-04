fcaea582c15de1cb221862daa27d4d8dc3cde12d
soorajshaji <soorajshaji6@gmail.com>
2022-09-04 23:27:02 +0530

    first laravel deployment to heroku

---
+++ database/migrations/2022_09_04_174807_form_index.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class FormIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customer_details', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->nullable();;
+            $table->integer('mid')->nullable();;
+            $table->integer('age')->nullable();;
+            $table->string('gender')->nullable();;
+            $table->string('phone_number')->nullable();;
+            $table->string('whatsapp_number')->nullable();;
+            $table->string('email')->nullable();;
+            $table->text('family_members')->nullable();;
+            $table->string('month_year')->nullable();;
+            $table->string('contribution')->nullable();;
+            $table->text('details')->nullable();;
+            $table->text('description')->nullable();;
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
