3a8225f2dcd95de15b7fe21a3d645c126ef10a02
soorajshaji <soorajshaji6@gmail.com>
2022-09-04 23:53:59 +0530

    added postgres database configuration

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'https://vast-citadel-16115.herokuapp.com/'),
+    'url' => env('APP_URL', 'http://localhost'),
 
     'asset_url' => env('ASSET_URL', null),
 
